c726f5ea12acf4ab2e70a40fc8b9b326f57ee7b3
geunyeol.ryu <rgyenr2@gmail.com>
2020-01-09 18:01:19 +0900

    add new page

---
+++ precache-manifest.993dacb73c31bf93a9d173fc23be365b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3afe41eb4440609cec6b6e5ff9c3ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3713fe43c33df046aeab",
+    "url": "/static/css/main.a029f903.chunk.css"
+  },
+  {
+    "revision": "296bccdf9a5ad9d3eb8a",
+    "url": "/static/js/2.213cf488.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.213cf488.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3713fe43c33df046aeab",
+    "url": "/static/js/main.869e686d.chunk.js"
+  },
+  {
+    "revision": "1752ba5a9b82c600d46f",
+    "url": "/static/js/runtime-main.141ad1e6.js"
+  }
+]);
